8623d17bfad330a80ad20a78c8c86bfeb15b9890
JeanFrancoisRetief <2458318@students.wits.ac.za>
2024-04-11 12:05:33 +0200

    Interact Triggers, extra room

--- Assets/Scripts/3D/Day1/Global.cs
+++ Assets/Scripts/3D/Day1/Global.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Global : MonoBehaviour
 {
     public int DialogueCounter;
+    //public int InteractCounter;
     // Start is called before the first frame update
     void Start()
     {
         DialogueCounter = 0;
-    }
+        //InteractCounter = 0;
+    } 
 
     // Update is called once per frame
     void Update()
