c9fcc1141d6ed253bd350dea31d63637821b8b5c
JeanFrancoisRetief <2458318@students.wits.ac.za>
2024-06-14 19:48:57 +0200

    Added Credits video

---
+++ Assets/VIDEO/CreditsScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsScript : MonoBehaviour
+{
+    public GameObject CreditsText;
+
+    public int framecount;
+
+    [SerializeField]
+    public int seconds;
+
+    void Start()
+    {
+        framecount = 0;
+        seconds = 0;
+        
+    }
+    private void Update()
+    {
+
+        CreditsText.transform.Translate(Vector3.up);
+
+        framecount++;
+        if (framecount >= 60)
+        {
+            seconds++;
+            framecount = 0;
+        }
+
+        if(seconds >= 60)
+        {
+            //Debug.Log("Exit Program");
+            Application.Quit();
+        }
+
+    }
+
+
+}
